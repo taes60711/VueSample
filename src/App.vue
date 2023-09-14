806a5c57a19e53b416ee6462c1d6c34516b7e899
chechi992 <miyu19980902@gmail.com>
2023-09-14 16:09:10 +0800

    0914 拆vue結構

--- src/App.vue
+++ src/App.vue
@@ -1,81 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <button @click="addData()">firebase_Create</button>
-    <br />
-    <button @click="getData()">firebase_Read</button>
-    <br />
-    <button @click="updateData()">firebase_Update</button>
-    <br />
-    <button @click="deleteData()">firebase_Delete</button>
-    <br />
-    <button @click="click()">Class</button>
-    <br />
-  </div>
+  <UserData
+  ></UserData>
 </template>
 <script>
-import { crudService } from "/src/Service/crudService.js";
-import { users } from "/src/Components/UserClass.js";
+import UserData from './Components/UserData.vue'
 
 export default {
   name: "App",
-  
+  components: {
+    UserData
+  },
   methods: {
     toMainPage: function () {
       this.$router.push({ name: "MainPage" });
-    },
-    //class
-    click() {
-      const userClass = new users();
-
-      console.log(userClass.getUser());
-
-      userClass.setUser({
-        Id: "classedId",
-        born: "classedborn",
-      });
-      console.log(userClass.getUser());
-      userClass.setUser({
-        Id: "classedId",
-        born: "classedborn",
-        first: "classedfirst",
-        last: "classedlast",
-      });
-      console.log(userClass.getUser());
-    },
-
-    //crudService.js  setData()
-    addData: async function () {
-      const crud = new crudService();
-      await crud
-        .setData()
-        .then(() => console.log("add Sucessfull"))
-        .catch((e) => console.error(e));
-    },
-
-    //crudService.js  getData()
-    getData: async function () {
-      const crud = new crudService();
-      await crud.getData().catch((e) => console.error(e));
-    },
-
-    //crudService.js  updateData()
-    updateData: async function () {
-      const crud = new crudService();
-      await crud
-        .updateData()
-        .then(() => console.log("update Sucessfull"))
-        .catch((e) => console.error(e));
-    },
-
-    //crudService.js  deleteData()
-    deleteData: async function () {
-      const crud = new crudService();
-      await crud
-        .deleteData()
-        .then(() => console.log("delete Sucessfull"))
-        .catch((e) => console.error(e));
     },
   },
 };
